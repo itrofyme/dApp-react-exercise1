c0b3820d3d18464a95cf7d31382a0d471e7585c5
itrofyme <ivantrofymenko@gmail.com>
2021-04-26 21:32:22 -0400

    Delete Button.js

--- src/components/Button.js
+++
@@ -1,9 +0,0 @@
-const Button = ({color, text, onClick}) => {
-    return (
-        <button className="btn" style={{ backgroundColor: color }} onClick={onClick}>
-            {text}
-        </button>
-    )
-}
-
-export default Button
