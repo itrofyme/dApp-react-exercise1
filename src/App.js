a85b05df85076736077cfcad7829856053aa69d5
Ivan Trofymenko <ivantrofymenko@gmail.com>
2021-05-06 13:30:48 -0400

    implemented avatar upload and made some small fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import Header from "./components/Header/Header";
 import Players from "./components/Players/Players";
-import LoginDialog from './components/LoginDialog/LoginDialog';
+import LoginDialog from "./components/LoginDialog/LoginDialog";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import styles from "./App.css";
@@ -9,75 +9,95 @@ import firebase from "./components/firebase";
 
 function App() {
   const initColorsInUse = {
-    "none": { value: "", enabled: true },
-    "red": { value: "red", enabled: true },
-    "green": { value: "green", enabled: true },
-    "blue": { value: "blue", enabled: true },
-    "purple": { value: "purple", enabled: true },
+    none: { value: "", enabled: true },
+    red: { value: "red", enabled: true },
+    green: { value: "green", enabled: true },
+    blue: { value: "blue", enabled: true },
+    purple: { value: "purple", enabled: true },
   };
 
-  const [ isLoginOpen, setIsLoginOpen ] = useState(false);
-  const [ fetchedColors, setFetchedColors ] = useState([]);
-  const [ initializing, setInitializing ] = useState(true);
-  const [ user, setUser ] = useState(); 
-  const [ colorsInUse, setColorsInUse ] = useState(JSON.parse(JSON.stringify(initColorsInUse)));
+  const [isLoginOpen, setIsLoginOpen] = useState(false);
+  const [fetchedColors, setFetchedColors] = useState([]);
+  const [initializing, setInitializing] = useState(true);
+  const [user, setUser] = useState();
+  const [avatarUrl, setAvatarUrl] = useState(null);
+  const [colorsInUse, setColorsInUse] = useState(
+    JSON.parse(JSON.stringify(initColorsInUse))
+  );
 
   const ref = firebase.firestore().collection("player_colors");
 
   function fetchColors() {
-    setInitializing(true)
+    setInitializing(true);
     ref.onSnapshot((querySnapshot) => {
       const items = [];
       querySnapshot.forEach((doc) => {
         items[doc.id] = doc.data();
       });
-      console.log('items : ', items);
+      console.log("items : ", items);
       setFetchedColors(items);
-      updateColorsInUseState(items);
-      setInitializing(false);
     });
   }
 
-  function updateColorsInUseState(items) {
+  function getUserAvatarUrl() {
+    if (user && fetchedColors.length) {
+      let playerId = user.email.substring(6, 7);
+      console.log("setting avatar_url", fetchedColors[playerId]["avatar_url"]);
+      setAvatarUrl(fetchedColors[playerId]["avatar_url"]);
+    }
+  }
+
+  function updateUserPhoto(url) {
+    const callableReturnMessage = firebase
+      .functions()
+      .httpsCallable("updateUserPhoto");
+    let playerId = user.email.substring(6, 7);
+    callableReturnMessage({ playerId: playerId, image: url })
+      .then((result) => {
+        console.log("result: ", result.data);
+      })
+      .catch((error) => {
+        console.log("error: ", JSON.stringify(error));
+      });
+  }
+
+  function updateColorsInUseState() {
     let newColorsInUse = JSON.parse(JSON.stringify(initColorsInUse));
     for (const [colorId, colorObj] of Object.entries(colorsInUse)) {
-      for (const [playerId, fetchedColorObj] of Object.entries(items)) {
+      for (const [playerId, fetchedColorObj] of Object.entries(fetchedColors)) {
         if (colorId === fetchedColorObj.color) {
-          console.log('colorId ', colorId, ' is disabled')
+          console.log("colorId ", colorId, " is disabled");
           newColorsInUse[colorId].enabled = false;
           break;
         }
-      } 
+      }
     }
     setColorsInUse(newColorsInUse);
-    console.log('colorsInUse : ', colorsInUse)
   }
 
   function changeColor(playerId, colorName) {
     var docRef = ref.doc(playerId.toString());
 
-    // Set the "capital" field of the city 'DC'
-    return docRef.update({
-        color: colorName
-    })
-    .then(() => {
+    return docRef
+      .update({
+        color: colorName,
+      })
+      .then(() => {
         console.log("updated player", playerId, " to ", colorName);
-    })
-    .catch((error) => {
+      })
+      .catch((error) => {
         console.error("error updating color: ", error);
-    });  
+      });
   }
 
-  useEffect(() => {
-    console.log('fetching colors ...')
-    fetchColors();
-  }, []);
-
   function onAuthStateChanged(user) {
-    console.log('onAuthStateChanged user: ', user);
     setUser(user);
     if (initializing) setInitializing(false);
   }
+
+  useEffect(() => {
+    fetchColors();
+  }, []);
 
   useEffect(() => {
     const auth = firebase.apps[0].auth();
@@ -85,37 +105,53 @@ function App() {
     return subscriber;
   }, []);
 
+  useEffect(() => {
+    updateColorsInUseState();
+    getUserAvatarUrl();
+    if (initializing) setInitializing(false);
+  }, [user, fetchedColors]);
+
   const handleLoginDialogOpen = () => {
     setIsLoginOpen(true);
-  }
+  };
 
   const handleLoginDialogClose = () => {
     setIsLoginOpen(false);
-  }
+  };
 
   const handleLogOut = () => {
     const auth = firebase.apps[0].auth();
     auth.signOut();
-  }
+    setAvatarUrl(null);
+  };
 
   if (initializing) {
-    return (
-      <Typography variant="h6">
-        Loading...
-      </Typography>
-    );
+    return <Typography variant="h6">Loading...</Typography>;
   } else {
     return (
       <>
         <Container className={styles.root} maxWidth="sm">
-          <Header user={user} handleLoginDialogOpen={handleLoginDialogOpen} handleLogOut={handleLogOut}/>
-          <Players user={user} fetchedColors={fetchedColors} colorsInUse={colorsInUse} changeColor={changeColor}/>
+          <Header
+            user={user}
+            handleLoginDialogOpen={handleLoginDialogOpen}
+            handleLogOut={handleLogOut}
+            updateUserPhoto={updateUserPhoto}
+            avatarUrl={avatarUrl}
+          />
+          <Players
+            user={user}
+            fetchedColors={fetchedColors}
+            colorsInUse={colorsInUse}
+            changeColor={changeColor}
+          />
         </Container>
-        <LoginDialog isOpen={isLoginOpen} handleClose={handleLoginDialogClose}></LoginDialog>
+        <LoginDialog
+          isOpen={isLoginOpen}
+          handleClose={handleLoginDialogClose}
+        ></LoginDialog>
       </>
     );
   }
-
 }
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,61 @@
+import { useState } from "react";
 import Typography from "@material-ui/core/Typography";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
+import Avatar from "@material-ui/core/Avatar";
 import styles from "./Header.module.css";
+import firebase from "../firebase";
 
-const Header = ({ user, handleLoginDialogOpen, handleLogOut }) => {
+const Header = ({
+  user,
+  handleLoginDialogOpen,
+  handleLogOut,
+  updateUserPhoto,
+  avatarUrl,
+}) => {
   const isLoggedIn = user !== null;
+  const [image, setImage] = useState(null);
+
+  console.log("avatarUrl", avatarUrl);
+
+  const handleImageUpload = (event) => {
+    if (event.target.files[0]) {
+      console.log(event.target.files[0]);
+      setImage(event.target.files[0]);
+    }
+  };
+
+  const handleUpload = () => {
+    const uploadTask = firebase
+      .storage()
+      .ref("images/" + image.name)
+      .put(image);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log("upload error: ", error);
+      },
+      () => {
+        firebase
+          .storage()
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            console.log(url);
+            updateUserPhoto(url);
+          });
+      }
+    );
+  };
+
+  if (image !== null) {
+    handleUpload();
+    setImage(null);
+    console.log("uploading image");
+  }
 
   return (
     <AppBar position="static">
@@ -13,8 +63,33 @@ const Header = ({ user, handleLoginDialogOpen, handleLogOut }) => {
         <Typography variant="h6" className={styles.loginButton}>
           Game Lobby App
         </Typography>
-        <Button color="inherit" onClick={handleLoginDialogOpen} style={{ display: isLoggedIn ? 'none' : undefined }}>Login</Button>
-        <Button color="inherit" onClick={handleLogOut} style={{ display: !isLoggedIn ? 'none' : undefined }}>Log Out</Button>
+        <Button
+          color="inherit"
+          onClick={handleLoginDialogOpen}
+          style={{ display: isLoggedIn ? "none" : undefined }}
+        >
+          Login
+        </Button>
+        <Button
+          color="inherit"
+          onClick={handleLogOut}
+          style={{ display: !isLoggedIn ? "none" : undefined }}
+        >
+          Log Out
+        </Button>
+        <Button
+          variant="contained"
+          component="label"
+          style={{ display: (avatarUrl || !user) ? "none" : undefined }}
+        >
+          Upload Avatar
+          <input type="file" hidden onChange={handleImageUpload} />
+        </Button>
+        <Avatar
+          src={avatarUrl}
+          className={styles.avatar}
+          style={{ display: !avatarUrl ? "none" : undefined }}
+        />
       </Toolbar>
     </AppBar>
   );
